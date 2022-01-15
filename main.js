b13aed1eae386da099d87ab9b25ac3e7048ab28c
thetaphobe <eatthemappleskappy@gmail.com>
2022-01-15 14:49:29 +0700

    delete

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ var clock = new THREE.Clock();
 
 let camera, scene, renderer, composer, helper;
 
-let modelFile = "./models/PinkReaper.pmx";
+let modelFile = "./models/Reaper Lolita Pink.pmx";
 let audioFile = "./mmd/Circus.wav";
 let motionFile = "./mmd/Circus-Motion.vmd";
 let cameraFile = "./mmd/Circus-Camera.vmd"; 
